1fc78a5b0023e3a15cbfa30cf17274232fad9cb7
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-06-29 12:24:59 +0530

    Dependencies is added

---
+++ Snapstagram/src/main/java/com/jyani/SnapstagramApplication.java
@@ -0,0 +1,13 @@
+package com.jyani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapstagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapstagramApplication.class, args);
+	}
+
+}
---
+++ Snapstagram/src/test/java/com/jyani/SnapstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapstagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
