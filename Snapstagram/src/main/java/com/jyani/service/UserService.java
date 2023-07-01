1d63b7a16395b9a44dae4a8ea8cba86aca044a73
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-07-01 09:41:23 +0530

    User List Handler is created and sucessfully implemented and still white list

--- Snapstagram/src/main/java/com/jyani/service/UserService.java
+++ Snapstagram/src/main/java/com/jyani/service/UserService.java
@@ -1,10 +1,15 @@
 package com.jyani.service;
 
+import java.util.List;
+
 import com.jyani.exception.UserException;
 import com.jyani.model.User;
 
 public interface UserService {
    
 	public User createUser(User user) throws UserException;
+	
+	public List<User> getAllUsers() throws UserException;
+	
 	
 }
